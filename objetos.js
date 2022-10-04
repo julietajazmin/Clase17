de672cd8a42d35df498a028e57f43133a273ff12
julietajazmin <julietajazmin1117@gmail.com>
2022-10-04 19:44:37 -0300

    first commit

---
+++ objetos.js
@@ -0,0 +1,78 @@
+/*function unaFuncion(params) {
+
+
+    return params;
+}
+
+let objeto= new Object ();
+
+let objeto2 = {
+    nombre: "Juan",
+    apellido: "Perez",
+    edad: 27
+    saludar: function() {
+        return "Hola mundo" + this.apellido +"" +this.nombre;
+    }
+//analizar saludar por qué no me deja
+}
+
+console.log(objeto2);
+console.log (objeto2.nombre);
+console.log(unaFuncion("Hola Mundo"));*/
+
+
+/*let cadena = "    Hola mundo lorem ipsum";
+
+console.log(cadena);
+console.log(cadena [3]);
+console.log (cadena.length); //longitud de la cadena
+console.log(cadena.slice(0,4)); //corta la cadena e indica donde empieza y donde temrina
+console.log(cadena.trim()); //quita los espacios en blanco
+
+let frutas = cadena.split(",");
+console.log(frutas);
+
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);*/
+
+/*let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let array2 = ["hola", "mundo", ];
+
+
+console.log (array);
+console.log(array2);
+
+
+console.log(array.length); //longitud del array
+console.log(array2 [array2.length-1]); //muestra el último elemento del aray
+
+array.push (11);//añade un elemnto al final del  array
+console.log (array);
+
+array2.push("julieta")
+console.log (array2);
+
+let array3 = array.concat(array2);
+console.log(array3);
+
+console.log(array2.includes ("Mundos")); //devuelve true si exste el elemento existe
+
+
+//no entiendo porque no me deja usar esto
+let frase = " lore ipsum dolor sit aemy";
+
+let vocales = "aeiou";
+
+let c = 0;
+    for (let i = 0; i< frase.length; i++){
+        if (vocales.includes {frase [i]}) {
+            c++;
+        }
+
+
+    }
+
+    console.log (`Esta frase tiene tantas vocales: $ {c}`);*/
+
